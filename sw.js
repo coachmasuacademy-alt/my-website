929ecc41bc925955eddb3c1c03545283b7aa0e91
coachmasuacademy-alt <coachmasuacademy@gmail.com>
2025-12-18 15:54:41 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // ==========================================
 
 //⚠️ アプリを更新する時は、必ずここのバージョン番号を上げてください (v1 -> v2 -> v3...)
-const CACHE_NAME = 'smart-coach-v6';
+const CACHE_NAME = 'smart-coach-v7';
 
 // キャッシュするファイル（オフラインで動くために必要なもの）
 const urlsToCache = [
@@ -85,4 +85,5 @@ self.addEventListener('message', (event) => {
     self.skipWaiting();
   }
 });
+
 
