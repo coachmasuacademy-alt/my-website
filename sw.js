265f262c9933192c198ca57df7f510616ad73b9b
coachmasuacademy-alt <coachmasuacademy@gmail.com>
2026-03-17 13:32:26 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Service Worker (Smart Coach Badminton v33)
 // ==========================================
 
-const CACHE_NAME = 'smart-coach-v37';
+const CACHE_NAME = 'smart-coach-v38';
 
 // キャッシュする最小限の静的アセット
 const urlsToCache = [
