b4c9391fb874a4d607db771514edbb10863ba37d
coachmasuacademy-alt <coachmasuacademy@gmail.com>
2026-08-29 07:19:18 +0900

    Update service worker to version 46

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Service Worker (Smart Coach Badminton v33)
 // ==========================================
 
-const CACHE_NAME = 'smart-coach-v45';
+const CACHE_NAME = 'smart-coach-v46';
 
 // キャッシュする最小限の静的アセット
 const urlsToCache = [
